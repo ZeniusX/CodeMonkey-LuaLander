3861f91492c059cfe69538d848a315835bf23028
Justine Brylle VIllacarlos <134256171+ZeniusX@users.noreply.github.com>
2025-08-19 12:40:30 +0800

    Update

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -6,6 +6,10 @@ public class Lander : MonoBehaviour
     [SerializeField] private float force = 700f;
     [SerializeField] private float turnSpeed = 100;
 
+    [Space]
+    [SerializeField] private float landingVelocityMagnitude = 4;
+    [SerializeField] private float minDotVector = 4;
+
     private Rigidbody2D landerRigidbody2D;
 
     private void Awake()
@@ -21,13 +25,29 @@ public class Lander : MonoBehaviour
         }
         if (Keyboard.current.aKey.isPressed)
         {
-            float turnSpeed = +100;
-            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            landerRigidbody2D.AddTorque(+turnSpeed * Time.deltaTime);
         }
         if (Keyboard.current.dKey.isPressed)
         {
-            float turnSpeed = -100;
-            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            landerRigidbody2D.AddTorque(-turnSpeed * Time.deltaTime);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.relativeVelocity.magnitude > landingVelocityMagnitude)
+        {
+            Debug.Log("Landed too hard!");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        if (dotVector < minDotVector)
+        {
+            Debug.Log("Landed on a steep angle");
+            return;
+        }
+
+        Debug.Log("Successful landing!");
     }
 }
